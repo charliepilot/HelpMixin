772ae09c411e259a76b009c34ea210f36b11bd0a
Kim Mathison <charliepilot@gmail.com>
2023-07-24 18:13:06 -0500

    initial gitignore

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='pyhelpmixin',
+    version='0.0.1',
+    description='Returns class main comments as help text',
+    url='https://github.com/shuds13/pyexample',
+    author='Kim Mathison',
+    author_email='',
+    license='',
+    packages=['pyhelpmixin'],
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        # 'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
